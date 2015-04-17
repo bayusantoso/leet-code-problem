c46ea7535e1fb28c6d0bd765049163f5fe3955a9
Bayu Santoso <bayusantoso.mail@gmail.com>
2015-04-18 06:36:37 +0700

    Commit Reverse Bit
    This is the accepted answer for problem Reverse Bit in Leet Code

---
+++ Reverse Bits/Reverse Bits/Reverse Bits/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sln = new Solution();
+            uint result = sln.reverseBits(2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public uint reverseBits(uint n)
+        {
+            uint result = 0;
+            string binary = convertToBinary(n);
+            result = convertToDecimal(binary);    
+            return result;
+        }
+
+        public string convertToBinary(uint n)
+        {
+            string binary = string.Empty;
+            int i = 0;
+            while (n > 0 || i < 32)
+            {
+                uint bin = n % 2;
+                binary = bin.ToString() + binary;
+                n = n / 2;
+                i++;
+            }
+            return binary;
+        }
+
+        public uint convertToDecimal(string binary)
+        {
+            uint result = 0;
+            for (int iteration = 0; iteration < binary.Length; iteration++)
+            {
+                uint bin = 0;
+                
+                if (binary[iteration] == Convert.ToChar("1"))
+                    bin = 1;
+
+                result += bin * Convert.ToUInt32(Math.Pow(2, Convert.ToDouble(iteration)));
+            }
+            return result;
+        }
+
+    }
+}
