709c16a4c47d5572c3b75da42871ab9ef4b8f74e
Bayu Santoso <bayusantoso.mail@gmail.com>
2015-04-17 23:37:40 +0700

    Commit New Solved Problem Set
    1. Length of Last Word
    2. Palindrome Number

---
+++ Length of Last Word/Length of Last Word/Length of Last Word/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Length_of_Last_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sln = new Solution();
+            Console.WriteLine(sln.LengthOfLastWord("H "));
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            if (String.IsNullOrEmpty(s)) return 0;
+            s = s.Trim();
+            string[] text = s.Split();
+            return text[text.Length - 1].Length;
+        }
+    }
+}
---
+++ Palindrome Number/Palindrome Number/Palindrome Number/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int palindrome = 0;
+            Solution sln = new Solution();
+            bool isPalindrome = sln.IsPalindrome(palindrome);
+
+            if (isPalindrome)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            
+            bool isPalindrome = true;
+            string text = x.ToString();
+
+            int end = text.Length - 1;
+            for (int iteration = 0; iteration < (text.Length / 2); iteration++, end--)
+            {
+                if (text[iteration] != text[end])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            return isPalindrome;
+        }
+    }
+}
